1bccc4f1d77bbcfc7eea22d2d3a1e70e6714dde8
Đinh Công Thành <dev.thanhdc@gmail.com>
2021-09-05 09:43:26 +0700

    Component Interaction 02

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { HelloComponent } from './hello/hello.component';
 import { FormsModule } from '@angular/forms';
 import { ProgressBarComponent } from './progress-bar/progress-bar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AuthorDetailComponent } from './author-detail/author-detail.component';
+import { AuthorListComponent } from './author-list/author-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HelloComponent,
-    ProgressBarComponent
+    ProgressBarComponent,
+    AuthorDetailComponent,
+    AuthorListComponent,
   ],
   imports: [
     BrowserModule,
